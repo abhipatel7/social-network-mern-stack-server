994a6bd419353ea05ac93884f52bb96f18bbb458
abhipatel7 <abhipatel7420@gmail.com>
2021-11-26 18:36:39 +0530

    register user complete

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,3 +1,40 @@
 import { Request, Response } from 'express';
 
-export const register = async (req: Request, res: Response) => {};
+import User from '../models/user';
+import { hashPassword } from '../helpers/utils';
+
+export const register = async (req: Request, res: Response) => {
+  try {
+    const { name, email, password, secret } = req.body;
+
+    if (!name) return res.status(400).send('Name is required!');
+
+    if (!password) return res.status(400).send('Password is required!');
+
+    if (password.length < 6 || password.length > 64)
+      return res
+        .status(400)
+        .send(
+          'Password must be between greater than 6 characters and less than 64 characters!'
+        );
+
+    if (!email) return res.status(400).send('Email is required!');
+
+    if (!secret) return res.status(400).send('Secret is required!');
+
+    const userExist = await User.findOne({ email });
+
+    if (userExist) return res.status(400).send('Email is taken!');
+
+    const hashedPassword = await hashPassword(password);
+
+    const user = new User({ name, email, password: hashedPassword, secret });
+
+    await user.save();
+
+    return res.json({ ok: true });
+  } catch (e) {
+    console.log('Registration Failed:', e.message);
+    return res.status(400).send('User registration failed!');
+  }
+};
--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -1,15 +1,6 @@
 import bcrypt from 'bcrypt';
 
 export const hashPassword = async (password: string) =>
-  //   return new Promise((resolve, reject) => {
-  //     bcrypt.genSalt(+process.env.SALT, (err, salt) => {
-  //       if (err) reject(err);
-  //       bcrypt.hash(password, salt, (err, hashedPassword) => {
-  //         if (err) reject(err);
-  //         resolve(hashedPassword);
-  //       });
-  //     });
-  //   });
   await bcrypt.hash(password, bcrypt.genSaltSync(+process.env.SALT));
 
 export const comparePassword = (password: string, hashedPassword: string) =>
